ac0292be826a4f7d1c057a75b0f46d33556fa3be
ehdrb01 <kdg1941@naver.com>
2024-09-30 18:10:03 +0900

    feat: 각 구조별 type 선언

--- byte/app/src/main/java/hgu/isel/reader/ByteReader.java
+++ byte/app/src/main/java/hgu/isel/reader/ByteReader.java
@@ -1,4 +1,24 @@
 package hgu.isel.reader;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ByteReader {
+    private final String inputPath;
+
+    public ByteReader(String inputPath) {
+        this.inputPath = inputPath;
+    }
+
+    public byte[] readClassFile() {
+        try {
+            return Files.readAllBytes(Paths.get(inputPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
