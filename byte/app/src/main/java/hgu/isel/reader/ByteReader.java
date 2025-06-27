4f4a3e785fd1ae99f045758a385d473bc3d416e8
ehdrb01 <kdg1941@naver.com>
2025-06-27 14:36:03 +0900

    create javadoc of the ByteReader class

--- byte/app/src/main/java/hgu/isel/reader/ByteReader.java
+++ byte/app/src/main/java/hgu/isel/reader/ByteReader.java
@@ -8,13 +8,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This ByteReader class is related to the File I/O tasks.
+ * By using the input path, which can be a path of directory or class file, it can read the entire bytecodes.
+ * Also, it can generate analyzed / tokenized bytecode files in txt format.
+ */
 public class ByteReader {
     private final String inputPath;
 
+    /**
+     * Constructor of the ByteReader.
+     * @param inputPath It is related to the 1) directory path or 2) single file path
+     */
     public ByteReader(String inputPath) {
         this.inputPath = inputPath;
     }
 
+    /**
+     * This method reads the entire single bytecode file.
+     * @return It returns the array of byte which represents the entire bytecodes.
+     */
     public byte[] readClassFile() {
         try {
 
@@ -26,6 +39,10 @@ public class ByteReader {
         return null;
     }
 
+    /**
+     * This method reads the entire class file paths from the input path. If there are 3 class files in the input directory, then it will return 3 paths.
+     * @return It returns List to represent all paths from the input directory.
+     */
     public List<String> readClassFilePaths() {
         Path startPath = Paths.get(inputPath);
         List<String> filePaths = new ArrayList<>();
@@ -47,6 +64,12 @@ public class ByteReader {
 
         return filePaths;
     }
+
+    /**
+     * This method reads the entire single bytecode file.
+     * @param path This method reads a single bytecode file which is in the path variable.
+     * @return It returns an array of byte which is a bytecode.
+     */
 
     public byte[] readClassFile(String path) {
         try {
